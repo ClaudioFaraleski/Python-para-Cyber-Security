b88c43d82b5d2c19ba2219745ebc0937e3007c31
ClaudioFaraleski <faraleski@hotmail.com>
2022-02-06 20:24:19 -0300

    conexao

---
+++ socket/cliente_tcp.py
@@ -0,0 +1,28 @@
+import socket
+import sys
+
+
+def main():
+    try:
+        conexao = socket.socket(socket.AF_INET,socket.SOCK_STREAM,0)
+    except socket.error as erro:
+        print("A conexão falhou !!!")
+        print("Erro :{}".format(erro))
+        sys.exit()
+
+    print("Conexão criada com seucesso")
+
+    try:
+        HostAlvo = input("Digite o IP ou Host a ser conectado :")
+        PortaAlvo = input("Digite a Porta a ser conectado:")
+
+
+        conexao.connect((HostAlvo , int(PortaAlvo)))
+        print(f'Cliente TCP conectado com sucesso  no host {HostAlvo} e na porta{PortaAlvo} ')
+        conexao.shutdown(2)
+
+    except socket.error as erro:
+        print("Falha na conexão ")
+
+
+
