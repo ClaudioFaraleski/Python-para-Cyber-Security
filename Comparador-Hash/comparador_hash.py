b6f178588976f4288ba21a505f3a4747034f82ea
ClaudioFaraleski <faraleski@hotmail.com>
2022-03-12 10:22:39 -0300

    Comparador de Hash

--- Comparador-Hash/comparador_hash.py
+++ Comparador-Hash/comparador_hash.py
@@ -1,26 +1,36 @@
 import hashlib
 
-
-doc1 = 'a.txt'
-doc2 = 'b.txt'
-
-
-hash1 = hashlib.new('ripemd160')
-
-hash1.update(open(doc1 ,'rb').read())
-
-hash2 = hashlib.new('ripemd160')
-
-hash2.update(open(doc2 ,'rb').read())
+try:
+    doc1 = input('Digite o Hash1 :')
+    doc2 = input('Digite o Hash 2 :')
 
 
-if hash1.digest() != hash2.digest():
-    print(f'O Hash1 {doc1} é diferente ao Hash2{doc2}')
-    print(f'o Hash do arquivo a.txt é  = {hash1.hexdigest()}')
-    print(f'o Hash do arquivo b.txt é  = {hash2.hexdigest()}')
-else:
-    print(f'O Hash1 {doc1} é igual ao Hash2{doc2}')
-    print(f'o Hash do arquivo a.txt é  = {hash1.hexdigest()}')
-    print(f'o Hash do arquivo b.txt é  = {hash2.hexdigest()}')
+    doc1 = 'a.txt'
+    doc2 = 'b.txt'
+
+    print('Tipos De Criptografia = md5 , ripemd160')
+    cripto = input('Digite o Tipo de Criptografia :').capitalize()
+
+
+
+    hash1 = hashlib.new(cripto)
+
+    hash1.update(open(doc1 ,'rb').read())
+
+    hash2 = hashlib.new('sha256')
+
+    hash2.update(open(doc2 ,'rb').read())
+
+
+    if hash1.digest() != hash2.digest():
+        print(f'O Hash1 {doc1} é diferente ao Hash2{doc2}')
+        print(f'o Hash do arquivo a.txt é  = {hash1.hexdigest()}')
+        print(f'o Hash do arquivo b.txt é  = {hash2.hexdigest()}')
+    else:
+        print(f'O Hash1 {doc1} é igual ao Hash2{doc2}')
+        print(f'o Hash do arquivo a.txt é  = {hash1.hexdigest()}')
+        print(f'o Hash do arquivo b.txt é  = {hash2.hexdigest()}')
+except:
+    print('Houve  erro na digitação tente de novo  ')
 
 
